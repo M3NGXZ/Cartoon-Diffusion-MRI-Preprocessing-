ab71d77cadbec445460bde5a2ddb6ed33a9fff27
Marcus Meng <105097110+M3NGXZ@users.noreply.github.com>
2022-08-08 14:09:53 -0400

    Delete dwi_copy_AP_in_dwi_folder.sh

--- dwi_copy_AP_in_dwi_folder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sub=$(</mnt/f/Neuroimaging/Actual_Data/Bash_Scripts/subject_list.txt);
-
-for i in $sub; do
-
-cp /mnt/f/Neuroimaging/Actual_Data/${i}/fmap/${i}_acq-01_dir-AP_run-02_epi.mif /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_mean_b0_AP.mif
-
-done
