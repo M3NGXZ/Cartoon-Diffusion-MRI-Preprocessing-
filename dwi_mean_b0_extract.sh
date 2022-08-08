7b1138a22ac012b7b5256fcf470007dbe2832757
Marcus Meng <105097110+M3NGXZ@users.noreply.github.com>
2022-08-08 14:10:39 -0400

    Delete dwi_mean_b0_extract.sh

--- dwi_mean_b0_extract.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sub=$(</mnt/f/Neuroimaging/Actual_Data/Bash_Scripts/subject_list.txt);
-
-for i in $sub; do
-
-dwiextract /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_dwi_den_unr_preproc_unbiased.mif - -bzero | mrmath - mean /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/mean_b0_preprocessed_${i}.mif -axis 3
-
-done
