4ea35f35de3eab40a8a295c5659949da1c60101a
Marcus Meng <105097110+M3NGXZ@users.noreply.github.com>
2022-08-08 14:10:45 -0400

    Delete dwi_mean_convert.sh

--- dwi_mean_convert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sub=$(</mnt/f/Neuroimaging/Actual_Data/Bash_Scripts/subject_list.txt);
-
-for i in $sub; do
-
-mrconvert /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/mean_b0_preprocessed_${i}.mif /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/mean_b0_preprocessed_${i}.nii.gz
-
-done
