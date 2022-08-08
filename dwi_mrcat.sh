ec9f7ccff453955f1d5055b0e3a4a5f3b0af24d5
Marcus Meng <105097110+M3NGXZ@users.noreply.github.com>
2022-08-08 14:10:50 -0400

    Delete dwi_mrcat.sh

--- dwi_mrcat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sub=$(</mnt/f/Neuroimaging/Actual_Data/Bash_Scripts/subject_list.txt);
-
-for i in $sub; do
-
-mrcat /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_mean_b0_AP.mif /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_mean_b0_PA.mif -axis 3 /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_b0_pair.mif
-
-done
