bab0ce3e03e7ed94d015477df6588ded2c299f85
Marcus Meng <105097110+M3NGXZ@users.noreply.github.com>
2022-08-08 14:11:01 -0400

    Delete dwi_mrtransform5tt.sh

--- dwi_mrtransform5tt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sub=$(</mnt/f/Neuroimaging/Actual_Data/Bash_Scripts/subject_list.txt);
-
-for i in $sub; do
-
-mrtransform /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/5ttgen_${i}.mif -linear /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/diff2struct_mrtrix_dof6_normalizedT1.txt -inverse /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_5tt_coreg.mif
-
-done
