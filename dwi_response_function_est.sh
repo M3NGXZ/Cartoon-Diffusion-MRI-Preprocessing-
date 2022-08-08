e101965fdac382a21badeb58a0fcb0a58c5a6dfc
Marcus Meng <105097110+M3NGXZ@users.noreply.github.com>
2022-08-08 14:11:16 -0400

    Delete dwi_response_function_est.sh

--- dwi_response_function_est.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sub=$(</mnt/f/Neuroimaging/Actual_Data/Bash_Scripts/subject_list.txt);
-
-for i in $sub; do
-
-dwi2response dhollander /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_dwi_den_unr_preproc_unbiased.mif /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_wm.txt /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_gm.txt /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_csf.txt -voxels /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_voxels.mif
-
-done
