2e69cdb4c5daf7e916f020d43a99d046966639a2
Marcus Meng <105097110+M3NGXZ@users.noreply.github.com>
2022-08-08 14:11:06 -0400

    Delete dwi_mrtransformT1.sh

--- dwi_mrtransformT1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sub=$(</mnt/f/Neuroimaging/Actual_Data/Bash_Scripts/subject_list.txt);
-
-for i in $sub; do
-
-mrtransform /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_desc-preproc_T1w_deface_deskull_ero.mif -linear /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/diff2struct_mrtrix_dof6_normalizedT1.txt -inverse /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_T1_coreg.mif
-
-done
