1ea246d1ae5c7e153d83e464cab357a785dc36b6
Marcus Meng <105097110+M3NGXZ@users.noreply.github.com>
2022-08-08 14:10:10 -0400

    Delete dwi_dwibiascorrect.sh

--- dwi_dwibiascorrect.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sub=$(</mnt/f/Neuroimaging/Actual_Data/Bash_Scripts/subject_list.txt);
-
-for i in $sub; do
-
-dwibiascorrect ants /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_dwi_den_unr_preproc.mif /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_dwi_den_unr_preproc_unbiased.mif -bias /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_bias.mif
-
-done
