496781500bfcff69b5f7cdbb01ce67478b753860
Marcus Meng <105097110+M3NGXZ@users.noreply.github.com>
2022-08-08 14:11:25 -0400

    Delete fmap_mrconvert.sh

--- fmap_mrconvert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sub=$(</mnt/f/Neuroimaging/Actual_Data/Bash_Scripts/subject_list.txt);
-
-for i in $sub; do 
-
-mrconvert /mnt/f/Neuroimaging/Actual_Data/${i}/fmap/${i}_acq-01_dir-AP_run-02_epi.nii -json_import /mnt/f/Neuroimaging/Actual_Data/${i}/fmap/${i}_acq-01_dir-AP_run-02_epi.json /mnt/f/Neuroimaging/Actual_Data/${i}/fmap/${i}_acq-01_dir-AP_run-02_epi.mif
-
-done
