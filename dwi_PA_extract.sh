ddb3f7d634736e4e1526c6536a041c14ed31488b
Marcus Meng <105097110+M3NGXZ@users.noreply.github.com>
2022-08-08 14:09:47 -0400

    Delete dwi_PA_extract.sh

--- dwi_PA_extract.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sub=$(</mnt/f/Neuroimaging/Actual_Data/Bash_Scripts/subject_list.txt);
-
-for i in $sub; do
-
-dwiextract /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_dwi_den_unr.mif - -bzero | mrmath - mean /mnt/f/Neuroimaging/Actual_Data/${i}/dwi/${i}_mean_b0_PA.mif -axis 3
-
-done
